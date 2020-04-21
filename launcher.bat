8471ff3709370d48bbe29135d9ee389ab1c533b7
Bohdan Tkachenko <bohdantk@buffalo.edu>
2020-04-21 10:30:57 -0400

    - launcher.bat: Fixed path

--- launcher.bat
+++ launcher.bat
@@ -1,7 +1,7 @@
 :: Change directory here to match file location:
 
-::cd C:\Users\tubxt2p\Documents\Python\python-projects\tictactoe\
-C:\Users\bohda\Desktop\Programming\Python\python-projects\tictactoe
+cd C:\Users\tubxt2p\Documents\Python\python-projects\tictactoe\
+::cd C:\Users\bohda\Desktop\Programming\Python\python-projects\tictactoe
 
 
 
