e0a1aa02985716a196a664db86aeb1885447fdfe
Bohdan Tkachenko <bohdantk@buffalo.edu>
2020-05-05 15:40:43 -0400

    - launcher.bat: Will now run from root directory instead of hardcoded path

--- launcher.bat
+++ launcher.bat
@@ -1,10 +1,5 @@
-:: Change directory here to match file location:
-
-cd C:\Users\tubxt2p\Documents\Python\python-projects\tictactoe\
-::cd C:\Users\bohda\Desktop\Programming\Python\python-projects\tictactoe
-
-
+:: Assign current working directory (root directory) to variable X
+set X=%CD%
 
 :: Change file name here
-
-python tictactoe.py
+python tic_tac_toe.py %X%
